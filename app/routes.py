c93dcebf05210e1f23a242b1a169e9c6a5e285c5
225164 <225164@student.uek.krakow.pl>
2023-06-07 12:59:10 +0200

    07.06 PB

--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,18 @@
 from app import app
+from flask import render_template
 
 @app.route('/')
-@app.route('index')
+@app.route('/index')
 def index():
-    return "Hello World!"
+    return render_template('index.html')
+
+@app.route('/opinion_extraction')
+def extract():
+    return render_template('opinion_extraction.html')
+
+@app.route('/product_list')
+def product_list():
+    return render_template('product_list.html')
 
 @app.route('/name/',defaults={'name': "Anonim"})
 @app.route('/name/<name>')
